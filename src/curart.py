6fea2fa7e26ac97c21a12f55a508b80e05dd3266
Tom Doyle <thomas.doyle9@mail.dcu.ie>
2019-03-28 14:59:40 +0000

    Added a tmp storage for images

--- src/curart.py
+++ src/curart.py
@@ -4,7 +4,11 @@ from flask import Flask, render_template, request
 import tlsh
 import urllib.request as ureq
 from werkzeug import secure_filename
+import os
 app = Flask(__name__)
+
+FILE_PREFIX = 'tmp/'
+
 
 @app.route('/upload')
 def upload_file():
@@ -15,15 +19,18 @@ def upload_filer():
    if request.method == 'POST':
       render_template('result.html')
       f = request.files['file']
-      print(f.filename)
-      f.save(secure_filename(f.filename))
+      f.save(f'{FILE_PREFIX}{secure_filename(f.filename)}')
 
-      img = im.load_img(f.filename)
+      # Process Image
+      img = im.load_img(f'{FILE_PREFIX}{f.filename}')
+      os.remove(f'{FILE_PREFIX}{f.filename}')
       kp, desc = im.get_keypoints(img)
       img_output = dm.pack_keypoints(kp, desc)
       img_hash = tlsh.hash(str(img_output).encode('utf-8'))
       result = dm.query_postgres(img_hash)
-      if len(result) > 1:
+
+      # Return Results
+      if len(result) > 0:
         url = result[0][1]
         ureq.urlretrieve(url, 'static/img/download.jpg')
         return render_template('result.html')
