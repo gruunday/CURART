9e2c3665d226cfd2324ff83801fb4b08c86d3645
Tom Doyle <thomas.doyle9@mail.dcu.ie>
2019-03-29 20:38:34 +0000

    updated test cases for get keypoints

--- src/tests/test_img_manipulation.py
+++ src/tests/test_img_manipulation.py
@@ -3,6 +3,22 @@ import img_manipulation as im
 
 class TestImgManip(unittest.TestCase):
 
+    def setUp(self):
+        self.img = im.load_img('testImages/upside.jpg')
+        with open('tests/data.txt', 'r') as f:
+            self.keypoints = f.readline()
+
     def test_load_img(self):
         self.assertEqual(str(type(im.load_img('testImages/upside.jpg'))), \
                               "<class 'numpy.ndarray'>")
+
+    def test_get_keypoints_len_one(self):
+        self.assertEqual(len(im.get_keypoints(self.img)[0]), 940)
+
+    def test_get_keypoints_len_two(self):
+        self.assertEqual(len(im.get_keypoints(self.img)[1]), 940)
+    
+    def test_get_keypoints_type(self):
+        key_lst = im.get_keypoints(self.img)[0]
+        for i in range(len(key_lst)):
+            self.assertEqual(str(type(key_lst[i])), "<class 'cv2.KeyPoint'>")
