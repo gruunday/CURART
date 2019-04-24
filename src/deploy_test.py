e74301c9a74213ad60fc7cf616447b9cbb6269ac
Tom Doyle <thomas.doyle9@mail.dcu.ie>
2019-04-24 14:14:03 +0100

    Fix deploy test script

--- src/deploy_test.py
+++ src/deploy_test.py
@@ -14,23 +14,14 @@ def send_alert(message):
     r = requests.post(config.slack_url, data=json.dumps(payload))
 
 def redeploy():
-    import os 
-    print(os.listdir("."))
-    os.system('chmod +x deploy.sh')
-    deploy_command = "ssh greenday@panoptes.xyz DBNAME=$DBNAME DBPASSWORD=$DBPASSWORD DBHOST=$DBHOST DBUSER=$DBUSER DBPORT=$DBPORT 'bash -s' < deploy.sh"
-    process = subprocess.Popen(deploy_command.split(), stdout=subprocess.PIPE)
-    return process.communicate()
+    sys.exit(435)
 
 def main():
     status = check_up()
     if status != 200:
         send_alert(f'Error responce from prod was {status} \n Trying to revert to last working commit')
         r.revert_commit()
-        output, error = redeploy()
-        if error:
-            send_alert(f'Error in redeploy {error}')
-        else:
-            send_alert(f'{output}')
+        redeploy()
     else:
         send_alert(f'Deployment Successful')
 
