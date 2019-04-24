89c7142e322478d9fd2312e73d04fa9f5bbe8d74
Tom Doyle <thomas.doyle9@mail.dcu.ie>
2019-04-24 14:20:47 +0100

    Fix deploy test script

--- src/deploy_test.py
+++ src/deploy_test.py
@@ -2,7 +2,7 @@ import requests
 import revert as r
 import json
 import config
-import sys
+import sys, os
 import subprocess
 
 def check_up():
@@ -14,7 +14,13 @@ def send_alert(message):
     r = requests.post(config.slack_url, data=json.dumps(payload))
 
 def redeploy():
-    sys.exit(435)
+    sys.path.append(os.getcwd())
+    #print(os.listdir("."))
+    os.system('chmod +x deploy.sh')
+    deploy_command = "ssh greenday@panoptes.xyz DBNAME=$DBNAME DBPASSWORD=$DBPASSWORD DBHOST=$DBHOST DBUSER=$DBUSER DBPORT=$DBPORT 'bash -s' < deploy.sh"
+    process = subprocess.Popen(deploy_command.split(), stdout=subprocess.PIPE)
+    return process.communicate()
+
 
 def main():
     status = check_up()
