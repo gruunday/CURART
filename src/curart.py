c93619fdfaf28f1fc333efaa9fc14c680c4412f9
Tom Doyle <thomas.doyle9@mail.dcu.ie>
2019-04-24 15:13:52 +0100

    Fixed broken file

--- src/curart.py
+++ src/curart.py
@@ -14,7 +14,7 @@ FILE_PREFIX = 'tmp/'
 
 @app.route('/')
 def upload_file():
-   return render_template('break.html')
+   return render_template('upload.html')
 	
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_filer():
