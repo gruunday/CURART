3223635d108033ea90f487d48ea5b09d77d7ac49
Tom Doyle <thomas.doyle9@mail.dcu.ie>
2019-04-24 09:51:26 +0100

    Added deployment test

---
+++ src/ops/deploy_test.py
@@ -0,0 +1,22 @@
+import requests
+import revert
+
+def check_up():
+    r = requests.get('https://upload.dtom.dev')
+    return r.status_code
+
+def send_alert():
+    def send_alert(message):
+    #### PROTECTED DO NOT COMMIT ####
+    slack_url = '{slack_url}'
+    r = requests.post(slack_url, data=json.dumps(message))
+
+def main():
+    status = check_up()
+    if status != 200:
+        send_alert(f'Error responce from prod was {status} \n Trying to revert to last working commit')
+    else:
+        send_alert(f'Deployment Successful')
+
+if __name__ == '__main__':
+    main()
---
+++ src/ops/revert.py
@@ -0,0 +1,39 @@
+import config
+import requests
+import json
+import subprocess
+import sys
+
+def get_last_commit():                                                                                                                            #### PROTECTED DO NOT COMMIT ####
+    r = requests.get('https://gitlab.computing.dcu.ie/api/v4/projects/doylet9%2F2019-ca400-doylet9/jobs?scope[]=success', headers={'PRIVATE-TOKEN': '{github_token}'})
+    parsed = json.loads(r.text)
+    return parsed[0]['commit']['id'], parsed[0]['finished_at']
+    
+def reset_head(last_commit):
+    gitResetCommand = f'git reset --hard {last_commit}'
+    process = subprocess.Popen(gitResetCommand.split(), stdout=subprocess.PIPE)
+    return process.communicate()
+
+def send_alert(message):
+    #### PROTECTED DO NOT COMMIT ####
+    slack_url = '{slack_url}'
+    r = requests.post(slack_url, data=json.dumps(message))
+
+def main():
+    last_commit, commit_time = get_last_commit()
+    reset_head(last_commit)
+    output, error = reset_head()
+    if not error:
+        text = "Encoutered error but reset to last known working version"
+    else:
+        text = f"PANIC: tried to reset to last known version but failed with error {error}"
+    message = {'text': f'{text}'}
+    send_alert(message)
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and sys.argv[1] == '-t':
+        text = 'Test Alert'
+        message = {'text': f'{text}'}
+        send_alert(message)
+    else:
+        main()
