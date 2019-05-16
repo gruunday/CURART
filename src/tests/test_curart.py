7e04dbfb27f69768e7e2c6d1f292c9c1890be7f6
Tom Doyle <thomas.doyle9@mail.dcu.ie>
2019-05-16 19:29:15 +0100

    Remvoed follow redirects

--- src/tests/test_curart.py
+++ src/tests/test_curart.py
@@ -17,6 +17,6 @@ class CurartTestCase(unittest.TestCase):
         d = {}
         with open('testImages/upside.jpg', 'rb') as f:
             d['file'] = (io.BytesIO(f.read()), 'testImages/upside.jpg')
-            rv = self.app.post('/uploader', data=d, follow_redirects=True, content_type='multipart/form-data')
+            rv = self.app.post('/uploader', data=d, content_type='multipart/form-data')
             assert rv.status_code == 200
 
